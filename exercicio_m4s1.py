449eb34e2596309a1ae658a95ecabe2f358acd2d
LUIZ HENRIQUE <luizhenriqueyhwh@gmail.com>
2022-12-17 21:10:18 -0300

    Adicionando exercicio_m4s1.py

---
+++ exercicio_m4s1.py
@@ -0,0 +1,44 @@
+import requests
+
+def get_modelos_veiculos(id_marca):
+    url = "https://parallelum.com.br/fipe/api/v1/carros/marcas/22/modelos"
+    headers = {'user-agent':'MyStudyApp'}
+    resposta = requests.get(url,headers=headers)
+    get_modelos_veiculos(id_marca)
+
+    
+
+    if resposta.status_code != 200:
+        print('Ouve um erro na requisição!!')
+
+    resposta_json = resposta.json()
+    return resposta_json['modelos']
+
+class ListaFipe():
+    def __init__(self,id_marca):
+        self.id_marca = id_marca
+        self.indice = 0
+        self.modelos = []
+
+    def __iter__(self):
+        self.indice >= get_modelos_veiculos(self.id_marca)
+        return self
+
+    def __next__(self):
+        if self.indice >= len(self.modelos):
+            raise StopIteration
+
+        modelo = self.modelos[self.indice]
+        self.indice += 1
+        return modelo
+
+lista_fipe = ListaFipe(22)
+
+for veiculo in lista_fipe:
+    print(f'nome:{veiculo["nome"]}')
+    print(f'ID: {veiculo["codigo"]}')
+    print("-"*50)
+
+
+
+
